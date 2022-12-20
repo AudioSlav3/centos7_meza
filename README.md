a2384bef1e5e440698ce3b9cc9280d38c90cf4e2
Mike L <mugen2u@gmail.com>
2022-12-20 16:25:24 -0600

    update code

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # centos7_meza
 
-Step 1. Install git<br>
-<code>sudo yum install -y git</code>
+Step 1. Log into VM as <b>root</b> and install git<br>
+<code>yum install -y git</code><br>
+
+Step 2. While still logged into VM as <b>root</b>, git clone repo<br>
+<code>git clone https://github.com/AudioSlav3/centos7_meza.git /tmp</code><br>
+
+Step 3. Run pre_init.sh
+<code>cd /tmp/</code><br>
+<code>bash /tmp/pre_init.sh</code><br>
--- pre_init.sh
+++ pre_init.sh
@@ -26,43 +26,74 @@ err="[${RED}ERR ${NC}] "
 warn="[${YELLOW}WARN${NC}] "
 #
 
+##### CHECK IF USER IS ROOT 
+if [ "$EUID" -ne 0 ]; then 
+  echo -e "${err}Please run as root"
+  exit 1
+fi
+adminuser=null
+
+#################################
+##### Add user to sudoers
+update_sudo () {
+ while ! test -f "${HOME}/sudo.done"; do 
+  sudouser=()
+  sudousers=$(cut -d: -f1 /etc/passwd | egrep -v "bin|daemon|adm|lp|sync|shutdown|halt|mail|operator|games|ftp|nobody|systemd-network|dbus|polkitd|sshd|postfix|root"|xargs)
+  for t in $sudousers; do 
+    sudouser+=( ${t} )
+  done
+  if [ ${#sudouser[@]} -ne 1 ]; then
+    echo echo -e "${err}Either too many users or insufficent users found.${NC}"
+	echo -e "${CYAN}Users found:${NC}"
+    printf "%s\n" "${sudouser[@]}"
+    echo -e "${info}#### PERFORM THE FOLLOWING AS ROOT ###${NC}"
+    echo -e "${info}## sudo visudo ${NC}"
+    echo -e "${info}## Add to end of file:${NC}"
+    echo -e "${info}## ${purple}${USER}  ALL=(ALL)       NOPASSWD: ALL${NC}"
+    echo -e "${info}## :wq${NC}"
+    echo -e "${info}######################################${NC}"
+    echo -e "${brown} Press [ENTER] ONLY AFTER completing ALL of the above.${NC}" 
+    read ans
+    sudo visudo
+    read ans
+  else 
+    sed -i 's/#includedir/includedir/g' /etc/sudoers
+    adminuser=${sudouser[0]}
+    echo "${sudouser[0]} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/${adminuser}
+    chown root:root /etc/sudoers.d/${adminuser}
+  fi
+  if $(cat /etc/sudoers | grep -q ${adminuser}) || test -f "/etc/sudoers.d/${adminuser}" ; then
+    touch ${HOME}/sudo.done
+  fi
+done
+sudo su - ${adminuser}
+}
 #################################
 ##### SSH Key
 update_ssh () {
+echo ${USER}
+read ans
 while ! test -f "${HOME}/ssh.done"; do 
-  echo -e "${info}#### PERFORM THE FOLLOWING ON YOUR HOST MACHINE ###${NC}"
-  echo -e "${info}## open pterm.exe (Putty windows command line) ${NC}"
-  echo -e "${info}## run: ${NC}"
-  echo -e "${info}## ${purple}pscp.exe ${white}..\..\NASAWiki\vmbuild\${purple}add_ssh.sh ${USER}@${hostname -I}:${HOME}${NC}"
-  echo -e "${info}######################################${NC}"
-  echo -e "${brown} Press [ENTER] ONLY AFTER completing ALL of the above.${NC}" 
-  read ans
+  if ! test -f "${HOME}/add_ssh.sh"; then
+    echo -e "${info}#### PERFORM THE FOLLOWING ON YOUR HOST MACHINE ###${NC}"
+    echo -e "${info}## open pterm.exe (Putty windows command line) ${NC}"
+    echo -e "${info}## run: ${NC}"
+    echo -e "${info}## ${purple}pscp.exe ${white}..\..\NASAWiki\vmbuild\${purple}add_ssh.sh ${USER}@${hostname -I}:${HOME}${NC}"
+    echo -e "${info}######################################${NC}"
+    echo -e "${brown} Press [ENTER] ONLY AFTER completing ALL of the above.${NC}" 
+    read ans
+  fi
   if test -f "${HOME}/add_ssh.sh"; then
     bash ${HOME}/add_ssh.sh
 	touch ${HOME}/ssh.done
-	echo -e "${update}Added SSH Key, you may verify your config by attempting to ssh with key.${NC}"
+    echo -e "${update}Added SSH Key, you may verify your config by attempting to ssh with key.${NC}"
   fi
 done
 }
 #################################
 ##### MISC Settings
 update_misc_settings () {
- while ! test -f "${HOME}/sudo.done"; do 
-  echo -e "${info}#### PERFORM THE FOLLOWING AS ROOT ###${NC}"
-  echo -e "${info}## sudo visudo ${NC}"
-  echo -e "${info}## Add to end of file:${NC}"
-  echo -e "${info}## ${purple}${USER}  ALL=(ALL)       NOPASSWD: ALL${NC}"
-  echo -e "${info}## :wq${NC}"
-  echo -e "${info}######################################${NC}"
-  echo -e "${brown} Press [ENTER] ONLY AFTER completing ALL of the above.${NC}" 
-  read ans
-  sudo visudo
-  read ans
-  if $(sudo cat /etc/sudoers | grep -q ${USER}); then
-    touch ${HOME}/sudo.done
-  fi
- done
- sudo cat << 'EOF' > /etc/issue
+sudo cat << 'EOF' > /etc/issue
 \S
 IP Address: \4
 EOF
@@ -74,13 +105,13 @@ update_packages () {
 	 myNewPackages=()
 	 sudo yum update -q
 	 centos_7_vbox="gcc make perl bzip2 kernel-headers-$(uname -r) kernel-devel-$(uname -r) elfutils-libelf-devel xorg-x11-drivers xorg-x11-util"
-	 centos_7=$centos_7_vbox" screen git nano zip unzip php74-pecl-zip.x86_64 dialog"
+	 centos_7=$centos_7_vbox" screen git nano zip unzip php74-pecl-zip.x86_64 dialog wget"
 	 
 	 centos_8_vbox="tar gcc make perl bzip2 kernel-headers-$(uname -r) kernel-devel-$(uname -r) elfutils-libelf-devel xorg-x11-drivers xorg-x11-utils.x86_64 libXt.x86_64"
 	 centos_8=$centos_8_vbox" git nano zip unzip dialog"
 	 
-	 centos_9_vbox="gcc make perl bzip2 kernel-headers-$(uname -r) kernel-devel-$(uname -r) elfutils-libelf-devel xorg-x11-drivers  xorg-x11-utils.x86_64  git nano zip unzip dialog wget"
-	 centos_9=$centos_9_vbox
+	 centos_9_vbox="gcc make perl bzip2 kernel-headers-$(uname -r) kernel-devel-$(uname -r) elfutils-libelf-devel xorg-x11-drivers  xorg-x11-utils.x86_64"
+	 centos_9=$centos_9_vbox" git nano zip unzip dialog wget"
 	 
 	 #Choose which centos version to pull from.
 	 centos_ver=$centos_7
@@ -126,7 +157,7 @@ install_VBoxGuest () {
  fi 
  done
 }
-
+update_sudo
 update_ssh
 update_misc_settings
 update_packages
