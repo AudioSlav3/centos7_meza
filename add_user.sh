b47b74840a951b0ebb4d6a63ba792bbe3e981e83
Mike L <mugen2u@gmail.com>
2022-12-22 10:37:38 -0600

    update code

--- add_user.sh
+++ add_user.sh
@@ -51,6 +51,6 @@ add_contributer () {
      echo -e "${update}Adding${cyan) ${usr} ${NC}to ${cyan}${t}${NC}"
      #WIKI=${t} php /opt/htdocs/mediawiki/maintenance/createAndPromote.php --force --bureaucrat --sysop --custom-groups=Contributor $usr $default_pswd
    done
-   echo -e "${info}${cyan}${usr}${NC} default password is${cyan} ${default_pswd} ${NC}"
+   #echo -e "${info}${cyan}${usr}${NC} default password is${cyan} ${default_pswd} ${NC}"
 }
 add_contributer $1
