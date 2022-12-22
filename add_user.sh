303c7a247450c9fc3fb752bf01d78fc6c439d1de
Mike L <mugen2u@gmail.com>
2022-12-22 10:32:26 -0600

    update code

--- add_user.sh
+++ add_user.sh
@@ -43,13 +43,13 @@ add_admin () {
 
 add_contributer () {
    usr=$1
-   default_pswd=$(date +%s | sha256sum | base64 | head -c 14 ; echo)
+   default_pswd="$(date +%s | sha256sum | base64 | head -c 14 ; echo)"
    wikis=()
    wikis=$(ls /opt/conf-meza/public/wikis/ | grep -v demo)
    for t in ${wikis[*]}; do 
      echo -e "${update}Adding${cyan) ${usr} ${NC}to ${cyan}${t}${NC}"
      #WIKI=${t} php /opt/htdocs/mediawiki/maintenance/createAndPromote.php --force --bureaucrat --sysop --custom-groups=Contributor $usr $default_pswd
    done
-   #echo -e "     ${info}${cyan}${usr}${NC}'s default password is: ' ${cyan}${default_pswd}${NC} ' ${NC}"
+   echo -e "     ${info}${cyan}${usr}${NC}'s default password is: ' ${cyan}${default_pswd}${NC} ' ${NC}"
 }
 add_contributer $1
